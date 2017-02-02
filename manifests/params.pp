79b97a67b5bbc5e42550dedf01a342fc08f57e43
Geoff Williams <geoff.williams@puppetlabs.com>
2017-02-02 11:09:16 +0800

    initial

---
+++ manifests/params.pp
@@ -0,0 +1,20 @@
+class cron::params {
+  case $facts["os"]["family"] {
+    "RedHat": {
+      $package    = "cronie-anacron"
+      $anacrontab = "/etc/anacrontab"
+      $crontab    = "/etc/crontab"
+      $cron_dirs  = [
+        "/etc/cron.hourly",
+        "/etc/cron.daily",
+        "/etc/cron.weekly",
+        "/etc/cron.monthly",
+        "/etc/cron.d",
+      ]
+    }
+    default: {
+      fail("Class ${name} does not support ${facts["os"]["family"]} yet")
+    }
+  }
+
+}
